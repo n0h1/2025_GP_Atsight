7c379c36acec20986001bf01de13c1eb04c8359a
Leenamh <146805161+Leenamh@users.noreply.github.com>
2025-01-28 01:30:17 +0300

    Initial Commit

---
+++ Atsight/AtsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtsightApp.swift
+//  Atsight
+//
+//  Created by lona on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct AtsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AtsightTests/AtsightTests.swift
@@ -0,0 +1,17 @@
+//
+//  AtsightTests.swift
+//  AtsightTests
+//
+//  Created by lona on 28/01/2025.
+//
+
+import Testing
+@testable import Atsight
+
+struct AtsightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
