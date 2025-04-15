7df47247bf107ae9497849af9dc6c43f0bcfdd16
najd08 <120324320+najd08@users.noreply.github.com>
2025-04-15 22:06:56 +0300

    Initial Commit

---
+++ AtSightWatchh Watch AppTests/AtSightWatchh_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AtSightWatchh_Watch_AppTests.swift
+//  AtSightWatchh Watch AppTests
+//
+//  Created by lona on 15/04/2025.
+//
+
+import Testing
+@testable import AtSightWatchh_Watch_App
+
+struct AtSightWatchh_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
